4e3a8e1930c31a8b538210aa0c79babc4ffbd891
Daniel Savard <savardd@gnulinux.ca>
2003-06-18 03:57:56 +0000

    Web counter.

--- version.js
+++ version.js
@@ -5,10 +5,12 @@ if (location.search) {
 	var ver = RegExp.$1;
 	document.writeln("You have LiveHTTPHeaders version <strong>" + ver + "</strong> installed!");
 	if (currentver > ver) {
-		document.writeln('<br><span class="warn">LiveHTTPHeaders version <strong>' + currentver + '</strong> is available!</span> You should upgrade to the newest version!');
+		document.writeln('<br><br><span class="warn">LiveHTTPHeaders version <strong>' + currentver + '</strong> is available!</span><br>You should upgrade to the newest version!');
 	} else if (currentver == ver) {
 		document.writeln("<br>You already have the latest version!");
 	}
+	document.images.afs.src = "http://www2.addfreestats.com/cgi-bin/connect.cgi?usr=00220215P008";
 } else {
 	document.writeln("LiveHTTPHeaders version <strong>" + currentver + "</strong> is available!");
 }
+
